af44576c83134c8720c43764dbd589bcf178abb2
Šarūnas Dubinskas <sarunasd@wix.com>
2019-04-15 22:13:42 +0300

    Adds declaration instructions

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Income Tax Calculator
 
+## Disclaimer
+
+"I will not take any responsibility if it will appear that I was wrong :)"
+
 ## How to use?
 
 1. Clone repo `git clone git@github.com:sarunas/income-tax-calculator.git`
 2. Run `npm install`
 3. Copy issued shares report data to `shares-issued.txt`
 4. Copy sold shares report data to `shares-sold.txt`
-5. Run `node index.js`
+5. Run `node index.js`, run `node index.js split-gain` if wanna split gain with partner
+6. Fill tax declaration
 6. Enjoy!
 
 ## Example
--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const { parseIssuedShares } = require('./lib/parse-issued-shares');
 const { parseSoldShares } = require('./lib/parse-sold-shares');
 const { generateReport } = require('./lib/generate-report');
+const { generateTaxFillInstructions } = require('./lib/generate-tax-fill-instructions');
 
 const issuedShares = parseIssuedShares(fs.readFileSync('./shares-issued.txt').toString());
 const soldShares = parseSoldShares(fs.readFileSync('./shares-sold.txt').toString());
 
 generateReport(issuedShares, soldShares).then(report => {
-  console.log(JSON.stringify(report));
+  generateTaxFillInstructions(report, process.argv.includes('split-gain'));
 });
