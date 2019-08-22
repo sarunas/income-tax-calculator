7f77d56fdddf8ed08b10565d1c31b6555a99e1dc
Kotryna Bulikaitė <kotrynab@wix.com>
2019-08-22 09:28:00 +0300

    FIxed naming

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { parseIssuedShares } = require('./lib/parse-issued-shares');
 const { parseSoldShares } = require('./lib/parse-sold-shares');
 const { generateReport } = require('./lib/generate-report');
 const { generateTaxFillInstructions } = require('./lib/generate-tax-fill-instructions-data');
-const { fetchExchangeRage } = require('./lib/currency-exchange');
+const { fetchExchangeRate } = require('./lib/currency-exchange');
 const { round } = require('./lib/round');
 
 module.exports = {
@@ -10,6 +10,6 @@ module.exports = {
   parseSoldShares,
   generateReport,
   generateTaxFillInstructions,
-  fetchExchangeRage,
+  fetchExchangeRate,
   round
 };
